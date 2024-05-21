59b2a67eab33a7ace953003d2e3817ffc8b20a46
Alexey <alexeydruz24@gmail.com>
2024-05-21 20:13:24 +0700

    test

--- sorts.cpp
+++ sorts.cpp
@@ -19,4 +19,38 @@ void selectionSort(int data[], int lenD)
  	data[i] = data[j];
  	data[j] = tmp;
  	}
-} 
+}
+
+void bubbleSort(int data[], int len)	
+{
+ 	int tmp = 0;
+ 	for(int i = 0; i < len; i++)	//проход по массиву
+ 	{
+		for(int j = len-1; j >= i+1; j--)	
+		{
+ 			if(data[j] < data[j-1])	//свап элементов
+			{						//по условию
+ 				tmp = data[j];
+ 				data[j] = data[j-1];
+ 				data[j-1] = tmp;
+ 			}
+ 		}
+ 	}
+}
+
+void insertionSort(int data[], int len)
+{
+ 	int key = 0;
+ 	int i = 0;
+ 	for(int j = 1; j < len; j++)	//проход по массиву
+	{
+ 		key = data[j];	//устновка ключа на элемент
+ 		i = j-1;
+ 		while(i >= 0 && data[i] > key)	//сдвиг элементов, которые больше
+		{								//текущего на одну позицию вправо
+ 			data[i+1] = data[i];
+ 			i = i - 1;
+ 		}
+		data[i+1] = key;
+ 	}
+}
