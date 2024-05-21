1f450b081bd31746533a7bce91e870c496e507d1
Фролова Анастасия Евгеньевна <ia231s17@csc.sibsutis.ru>
2024-05-21 21:30:33 +0700

    test main with a menu, an infinite loop, an array for sorting, and a timer

--- main.cpp
+++ main.cpp
@@ -3,8 +3,35 @@
 #include <random>
 
 using namespace std;
-
+// Функция для замера времени сортировки
 double timer(int tip_sort, int a[], long long n)
 {
 
     }
+
+int main()
+{
+    setlocale(LC_ALL, "rus"); // Устанавливаем локаль для корректного отображения русских символов
+    int m[100000]; // Объявляем массив для тестирования
+	long long choice, len; // Переменные для выбора типа сортировки и длины массива
+	double time; // Переменная для хранения времени выполнения сортировки
+
+    while (true) // Бесконечный цикл для повторения тестирования
+	{
+        // меню для выбора типа сортировки
+		cout << "Select the sort type:\n(0) exit\n(1) Sorting by choice\n(2)";
+		cin >> choice;
+
+		if (choice == 0)
+		return 0;
+        cout << "Write the length of the array under test:\n";// выбор длины тестируемого массива
+		cin >> len;
+
+        // Заполняем массив случайными числами
+		for (long long i = 0; i < len; i++)
+		m[i] = rand();
+
+        // Замеряем время выполнения сортировки и выводим результат
+		cout << timer(choice, m, len) << "s\n";
+    }
+}
