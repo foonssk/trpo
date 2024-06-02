adeecc9375bc18bd0f20432541c540ae39388961
Фролова Анастасия Евгеньевна <ia231s17@csc.sibsutis.ru>
2024-06-02 20:55:55 +0700

    теперь можно жоска выбирать тип сортировки уау

--- main.cpp
+++ main.cpp
@@ -2,57 +2,56 @@
 #include <ctime>
 #include <random>
 #include "sorts.h"
-using namespace std;
-// Функция для замера времени сортировки
-double timer(int tip_sort, int a[], long long n)
-{
-    unsigned int start = clock(); //выбор сортировки 
-	if (tip_sort == 1)
-	{
-	selectionSort(a, n);
-	}
-	else if (tip_sort == 2)
-	{
-	bubbleSort(a, n);
-	}
-	else if (tip_sort == 3)
-	{
-	insertionSort(a, n);
-	}
-	else if (tip_sort == 4)
-	{
-	mergeSort(a, n);
-	}
-	else
-	{
-	quickSort(a, n);
-	}
-	return (clock() - start) / 1000.0; //время в сек
-    }
 
-int main()
-{
+using namespace std;
+
+// Функция для замера времени сортировки
+double timer(int tip_sort, int a[], long long n) {
+    unsigned int start = clock(); // выбор сортировки 
+    if (tip_sort == 1) {
+        selectionSort(a, n);
+    } else if (tip_sort == 2) {
+        bubbleSort(a, n);
+    } else if (tip_sort == 3) {
+        insertionSort(a, n);
+    } else if (tip_sort == 4) {
+        mergeSort(a, n);
+    } else {
+        quickSort(a, n);
+    }
+    return (clock() - start) / 1000.0; // время в сек
+}
+
+int main() {
     setlocale(LC_ALL, "rus"); // Устанавливаем локаль для корректного отображения русских символов
     int m[100000]; // Объявляем массив для тестирования
-	long long choice, len; // Переменные для выбора типа сортировки и длины массива
-	double time; // Переменная для хранения времени выполнения сортировки
+    long long choice, len; // Переменные для выбора типа сортировки и длины массива
+    double time; // Переменная для хранения времени выполнения сортировки
 
-    while (true) // Бесконечный цикл для повторения тестирования
-	{
-        // меню для выбора типа сортировки
-		cout << "Select the sort type:\n(0) exit\n(1) Sorting by choice\n(2)";
-		cin >> choice;
+    while (true) { // Бесконечный цикл для повторения тестирования
+        // Меню для выбора типа сортировки
+        cout << "Выберите тип сортировки:\n";
+        cout << "(0) Выход\n";
+        cout << "(1) Сортировка выбором\n";
+        cout << "(2) Пузырьковая сортировка\n";
+        cout << "(3) Сортировка вставками\n";
+        cout << "(4) Сортировка слиянием\n";
+        cout << "(5) Быстрая сортировка\n";
+        cout << "Введите ваш выбор: ";
+        cin >> choice;
 
-		if (choice == 0)
-		return 0;
-        cout << "Write the length of the array under test:\n";// выбор длины тестируемого массива
-		cin >> len;
+        if (choice == 0)
+            return 0;
+
+        cout << "Введите длину тестируемого массива: ";
+        cin >> len;
 
         // Заполняем массив случайными числами
-		for (long long i = 0; i < len; i++)
-		m[i] = rand();
+        for (long long i = 0; i < len; i++)
+            m[i] = rand();
 
         // Замеряем время выполнения сортировки и выводим результат
-		cout << timer(choice, m, len) << "s\n";
+        time = timer(choice, m, len);
+        cout << "Время сортировки: " << time << " секунд\n";
     }
 }
