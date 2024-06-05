41355c098a0ef0abc1e96ab944b2c0f1a7f030e2
Alexey <alexeydruz24@gmail.com>
2024-06-06 01:20:54 +0700

    test

---
+++ Makefile
@@ -0,0 +1,68 @@
+CC = g++
+CFLAGS = -Wall -Werror
+CPPFLAGS = -I src -MP -MMD -I thirdparty
+LDFLAGS = -lm
+
+APP_NAME = password
+LIB_NAME = sorts
+TEST_NAME = main_test
+
+BIN_DIR = bin
+OBJ_DIR = obj
+SRC_DIR = src
+TEST_DIR = filetest
+DEPS_DIR = thirdparty
+
+APP_PATH = $(BIN_DIR)/$(APP_NAME)
+LIB_PATH = $(OBJ_DIR)/$(SRC_DIR)/$(LIB_NAME)/$(LIB_NAME).a
+TEST_APP = $(BIN_DIR)/$(TEST_NAME)
+
+SRC_EXT = cpp
+
+APP_SOURCES = $(wildcard $(SRC_DIR)/$(APP_NAME)/*.$(SRC_EXT))
+APP_OBJECTS = $(APP_SOURCES:$(SRC_DIR)/%.$(SRC_EXT)=$(OBJ_DIR)/$(SRC_DIR)/%.o)
+
+LIB_SOURCES = $(wildcard $(SRC_DIR)/$(LIB_NAME)/*.$(SRC_EXT))
+LIB_OBJECTS = $(LIB_SOURCES:$(SRC_DIR)/%.$(SRC_EXT)=$(OBJ_DIR)/$(SRC_DIR)/%.o)
+
+TEST_SOURCES = $(wildcard $(TEST_DIR)/*.$(SRC_EXT))
+TEST_OBJECTS = $(TEST_SOURCES:$(TEST_DIR)/%.$(SRC_EXT)=$(OBJ_DIR)/$(TEST_DIR)/%.o)
+
+TEST_LIBS = $(LIB_PATH)
+
+DEPS = $(APP_OBJECTS:.o=.d) $(LIB_OBJECTS:.o=.d) $(TEST_OBJECTS:.o=.d)
+
+all: $(APP_PATH)
+
+-include $(DEPS)
+
+$(APP_PATH): $(APP_OBJECTS) $(LIB_PATH)
+	$(CC) $(CFLAGS) $(CPPFLAGS) $^ -o $@ $(LDFLAGS)
+
+$(LIB_PATH): $(LIB_OBJECTS)
+	ar rcs $@ $^
+
+$(TEST_APP): $(TEST_OBJECTS) $(LIB_PATH)
+	$(CC) $(CFLAGS) $(CPPFLAGS) $^ -o $@ $(LDFLAGS)
+
+$(OBJ_DIR)/%.o: %.$(SRC_EXT)
+	$(CC) -c $(CFLAGS) $(CPPFLAGS) $< -o $@
+
+
+.PHONY: run
+all:
+	./$(APP_PATH)
+
+.PHONY: clean
+clean:
+	$(RM) $(APP_PATH) $(TEST_APP)
+	$(RM) $(APP_OBJECTS) $(LIB_OBJECTS) $(TEST_OBJECTS)
+	$(RM) $(OBJ_DIR)/$(SRC_DIR)/$(LIB_NAME)/*.a
+	$(RM) $(OBJ_DIR)/$(SRC_DIR)/**/*.o
+	$(RM) $(OBJ_DIR)/$(SRC_DIR)/**/*.d
+	$(RM) $(OBJ_DIR)/$(TEST_DIR)/*.o
+	$(RM) $(OBJ_DIR)/$(TEST_DIR)/*.d
+
+.PHONY: test
+test: $(TEST_APP)
+	./$(TEST_APP)
--- filetest/test.cpp
+++ filetest/test.cpp
@@ -1,81 +1,8 @@
-#include <cstring>
-#include <iostream>
-#include <sorts/sorts.h>
+#define CTEST_MAIN
 
-// Function to print an array
-void
-printArray (const int arr[], int size)
+#include "./../thirdparty/ctest.h"
+
+int main(int argc, const char** argv)
 {
-  for (int i = 0; i < size; i++)
-    {
-      std::cout << arr[i] << " ";
-    }
-  std::cout << std::endl;
-}
-
-// Function to check if an array is sorted
-bool
-isSorted (const int arr[], int size)
-{
-  for (int i = 1; i < size; i++)
-    {
-      if (arr[i - 1] > arr[i])
-        {
-          return false;
-        }
-    }
-  return true;
-}
-
-// Main function to test the sorting algorithms
-int
-main ()
-{
-  int data1[] = { 64, 25, 12, 22, 11 };
-  int data2[] = { 64, 25, 12, 22, 11 };
-  int data3[] = { 64, 25, 12, 22, 11 };
-  int data4[] = { 64, 25, 12, 22, 11 };
-  int data5[] = { 64, 25, 12, 22, 11 };
-  int data6[] = { 64, 25, 12, 22, 11 };
-  int len = 5;
-
-  std::cout << "Original array: ";
-  printArray (data1, len);
-
-  // Test selection sort
-  selectionSort (data1, len);
-  std::cout << "After selection sort: ";
-  printArray (data1, len);
-  std::cout << "Is sorted: " << (isSorted (data1, len) ? "True" : "False")
-            << std::endl;
-
-  // Test bubble sort
-  bubbleSort (data2, len);
-  std::cout << "After bubble sort: ";
-  printArray (data2, len);
-  std::cout << "Is sorted: " << (isSorted (data2, len) ? "True" : "False")
-            << std::endl;
-
-  // Test insertion sort
-  insertionSort (data3, len);
-  std::cout << "After insertion sort: ";
-  printArray (data3, len);
-  std::cout << "Is sorted: " << (isSorted (data3, len) ? "True" : "False")
-            << std::endl;
-
-  // Test merge sort
-  mergeSort (data4, len);
-  std::cout << "After merge sort: ";
-  printArray (data4, len);
-  std::cout << "Is sorted: " << (isSorted (data4, len) ? "True" : "False")
-            << std::endl;
-
-  // Test quick sort
-  quickSort (data5, len);
-  std::cout << "After quick sort: ";
-  printArray (data5, len);
-  std::cout << "Is sorted: " << (isSorted (data5, len) ? "True" : "False")
-            << std::endl;
-
-  return 0;
+    return ctest_main(argc, argv);
 }
